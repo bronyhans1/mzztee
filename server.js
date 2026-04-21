4c87ef1cefa204d9005d9327ff49ddf459c6438e
bronyhans1 <bronyoris@gmail.com>
2026-04-21 16:09:01 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bookingRoutes = require("./routes/bookingRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors(origin: {https://mzztee.vercel.app}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/bookings", bookingRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
